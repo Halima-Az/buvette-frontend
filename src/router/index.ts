a3636a5d53c42d1a76d629b536bf38ec02afd663
ahlam <halimaazioui17@gmail.com>
2025-11-13 22:47:36 +0100

    prepare vue js envirement

---
+++ src/router/index.ts
@@ -0,0 +1,22 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Landing from '@/views/Landing.vue'
+import Login from '@/views/Login.vue'
+import Register from '@/views/Register.vue'
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/home',
+      name: 'Landing',
+      component: Landing,
+    },
+    {
+      path:'/login',
+      name:'login',
+      component:Login
+    },
+    {path:'/register',name:'register',component:Register}
+  ],
+})
+
+export default router
